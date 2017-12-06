f6b85920d84563f4c14ec877e6fce764a15b9bd1
HaoxDev <hao0628@gmail.com>
2017-12-07 02:00:01 +0800

    first commit

---
+++ ReadFileToBlock.c
@@ -0,0 +1,298 @@
+#include <stdio.h>
+#include <math.h>
+#include <malloc.h>
+
+#define PI 3.141592653589793
+#define SQH 0.707106781186547  /* square root of 2 */
+#define SWAP(a,b)  tempr=(a); (a) = (b); (b) = tempr
+#define IMAGE_SIZE 512
+
+
+static void fft1();
+
+void dct1(x,n)
+float *x;
+int n;
+{
+  int i,ii,nn,mm;
+  float tc,ts,sqn,temp2;
+  double temp1;
+  float *v;
+  void fft1();
+
+  nn = n >> 1;
+  mm = n << 1;
+  sqn = (float)sqrt((double)n);
+
+  v = (float *) calloc (mm,sizeof(float));
+  if (v == NULL) {
+    printf("allocation failure\n");
+    exit(1);
+  }
+
+  for (i=0;i<nn;i++) {
+    ii = i << 1;
+    v[ii] = x[ii];
+    v[ii+1] = 0.0;
+  }
+  for (i=nn;i<n;i++) {
+    ii = i << 1; 
+    v[ii] = x[mm-ii-1];
+    v[ii+1] = 0.0;
+  }
+
+  fft1(v-1,n,1);
+
+  temp2 = SQH/sqn;
+  x[0] = v[0]/sqn;
+  for (i=1;i<=nn;i++) {
+    ii = i << 1;
+    temp1 = (double)(PI*i/mm);
+    tc = (float) cos(temp1);
+    ts = (float) sin(temp1);
+    x[i] = 2.0*(tc*v[ii] + ts*v[ii+1])*temp2;
+    x[n-i] = 2.0*(ts*v[ii] - tc*v[ii+1])*temp2;
+  }
+
+  free(v);
+}
+/* -------------------------------------------------- */
+
+void idct1(x,n)
+float *x;
+int n;
+{
+  int i,ii,mm,nn;
+  float *v;
+  float temp2,tc,ts,sqn;
+  double temp1;
+  void fft1();
+
+  nn = n >> 1;
+  mm = n << 1;
+  sqn = (float)sqrt((double)n);
+
+  v = (float *) calloc (mm,sizeof(float));
+  if (v == NULL) {
+    printf("allocation failure\n");
+    exit(1);
+  }
+
+  temp2 = sqn/SQH;  
+  v[0] = x[0]*sqn;
+  v[1] = 0.0;
+  for (i=1;i<n;i++) {
+    ii = i << 1;
+    temp1 = (double)(PI*i/mm);
+    tc = (float)cos(temp1);
+    ts = (float)sin(temp1);
+    v[ii] = 0.5*(tc*x[i] + ts*x[n-i])*temp2;
+    v[ii+1] = 0.5*(ts*x[i] - tc*x[n-i])*temp2;
+  }
+
+  fft1(v-1,n,-1);
+
+  for (i=0;i<nn;i++) {
+    ii = i << 1;
+    x[ii] = v[ii];
+  }
+  for (i=nn;i<n;i++) {
+    ii = i << 1;
+    x[mm-ii-1] = v[ii];
+  }
+  free(v);
+}
+/* -------------------------------------------------- */
+
+/* 1-D fft program  */
+/* Replace data by its discrete Fourier ransform if isign is input as 1,
+   or replace data by its inverse discrete Fourier transform if
+   isign is input as -1.  "data" is a complex array of length nn, input
+   as a real array data [1..2*nn], nn must be an integer power of 2.     */
+
+/* If your data array is zero-offset, that is the range of data is 
+   [0..2*nn-1], you have to decrease the pointer to data by one when
+   fft1 is invoked, for example fft1(data-1,256,1).
+   The real part of the first output will now be return in data[0],
+   the imaginary part in data[1] and so on.                              */
+
+static void fft1(data,nn,isign)
+float *data;
+int nn,isign;
+{
+    int n,mmax,m,j,istep,i;
+    double wtemp,wr,wpr,wpi,wi,theta;
+    float tempr,tempi;
+    n = nn << 1;
+    j = 1;
+    for (i=1;i<n;i+=2) {
+       if (j>i) {
+          SWAP(data[j],data[i]);
+          SWAP(data[j+1],data[i+1]);
+       }
+       m = n >> 1;
+       while (m>=2 && j>m) {
+         j -= m;
+         m >>= 1;
+       }
+       j += m;
+    }
+    mmax = 2;
+    while (n>mmax) {
+         istep = 2*mmax;
+         theta = 6.28318530717959/(-isign*mmax);
+         wtemp = sin(0.5*theta);
+         wpr = -2.0*wtemp*wtemp;
+         wpi = sin(theta);
+         wr = 1.0;
+         wi = 0.0;
+         for (m=1;m<mmax;m+=2) {
+            for (i=m;i<=n;i+=istep) {
+              j = i+mmax;
+              tempr = wr*data[j]-wi*data[j+1];
+              tempi = wr*data[j+1]+wi*data[j];
+              data[j] = data[i]-tempr;
+              data[j+1] = data[i+1]-tempi;
+              data[i] += tempr;
+              data[i+1] += tempi;
+            }
+            wr = (wtemp=wr)*wpr-wi*wpi+wr;
+            wi = wi*wpr+wtemp*wpi+wi;
+         }
+         mmax = istep;
+     }
+    
+     if (isign == -1) {
+        for (i=1;i<=n;++i)
+          data[i] = data[i]/nn;
+     }
+}
+
+void dct2(x,n)
+float **x;
+int n;
+{
+  int i,j;
+  float *y;
+  void dct1();
+
+  y = (float *) calloc (n,sizeof(float));
+  if (y == NULL) {
+   printf("allocation failure\n");
+   exit(1);
+  }
+  for (i=0;i<n;i++) {
+    for (j=0;j<n;j++) 
+      y[j] = x[j][i];
+    dct1(y,n);
+    for (j=0;j<n;j++) 
+      x[j][i] = y[j];
+  }   /* end of loop i */
+
+  for (i=0;i<n;i++) {
+    for (j=0;j<n;j++) 
+      y[j] = x[i][j];
+    dct1(y,n);
+    for (j=0;j<n;j++) 
+      x[i][j] = y[j];
+  }   /* end of loop i */
+
+  free(y);
+}
+
+/* ----------------------------------------------- */
+
+void idct2(x,n)
+float **x;
+int n;
+{
+  int i,j;
+  float *y;
+  void idct1();
+
+  y = (float *) calloc (n,sizeof(float));
+  if (y == NULL) {
+   printf("allocation failure\n");
+   exit(1);
+  }
+  for (i=0;i<n;i++) {
+    for (j=0;j<n;j++) 
+      y[j] = x[j][i];
+    idct1(y,n);
+    for (j=0;j<n;j++) 
+      x[j][i] = y[j];
+  }   /* end of loop i */
+
+  for (i=0;i<n;i++) {
+    for (j=0;j<n;j++) 
+      y[j] = x[i][j];
+    idct1(y,n);
+    for (j=0;j<n;j++) 
+      x[i][j] = y[j];
+  }   /* end of loop i */
+
+  free(y);
+}
+
+/* ----------------------------------------------- */
+
+
+unsigned char image_byte[IMAGE_SIZE][IMAGE_SIZE];
+
+
+//最左上為0,0
+//最右下為64,64
+assign_block(float** target,int x,int y){
+    int i,j;
+    for(i = 0; i < 8 ; i++){
+        for(j = 0 ; j < 8 ; j++){
+            target[i][j] = (float)image_byte[8*y+i][8*x+j];
+        }
+    }
+}
+
+float** get_block(int x,int y){
+    //allocation for block
+    float **block;
+    block = (float **) malloc(sizeof(float *)*8);
+    int i,j;
+    for(i = 0 ; i < 8 ; i++){
+        block[i] = (float *) malloc(sizeof(float) * 8);
+    }
+    assign_block(block,0,0);
+
+    return block;
+} 
+
+int main(int argc,char **argv){
+
+    // read and save a byte arr
+
+    char* input_fname = argv[1];
+    FILE *fin;
+
+    fin = fopen(input_fname,"rb");
+    int i,j;
+    unsigned char temp;
+    for( i = 0 ; i < IMAGE_SIZE ; i++){
+        for( j = 0 ; j < IMAGE_SIZE ; j++){
+            fread(&temp, sizeof(char), 1, fin);
+            image_byte[i][j] = temp;
+        }
+    }
+       
+    float **block = get_block(0,0);
+    
+
+    //dct2(block,8);
+
+    for(i = 0; i < 8 ; i++){
+        for(j = 0 ; j < 8 ; j++){
+            printf("%f\t",* (*(block+i)+j));
+        }
+        printf("\n");
+    }  
+
+}
+
+
